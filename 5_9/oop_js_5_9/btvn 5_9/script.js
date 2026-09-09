4e642edee993601b70e44e69b8cb60155986c8ac
KhanhBaoLe <khanhlebao26@gmail.com>
2026-09-09 11:09:01 +0700

    imporve

--- 5_9/oop_js_5_9/btvn 5_9/script.js
+++ 5_9/oop_js_5_9/btvn 5_9/script.js
@@ -1,50 +1,42 @@
 class Toys {
-    constructor(name){
-        this.name = name
+    constructor(name) {
+        // Tùy chọn: Ngăn tạo đối tượng trực tiếp từ class Toys (giả lập Abstract Class)
+        if (new.target === Toys) {
+            throw new Error("Không thể khởi tạo trực tiếp lớp Toys!");
+        }
+        this.name = name;
     }
-    
-    speak(){
 
+    speak() {
+        throw new Error("Phương thức speak() phải được định nghĩa ở lớp con!");
     }
 }
 
+// Bỏ constructor dư thừa, JS tự động gọi super(name)
 class RobotToy extends Toys {
-    constructor(name){
-        super(name);
-    }
-
-    speak(){
+    speak() {
         console.log(`${this.name} says beep beep book! I am a robot!`);
     }
 }
 
-class TeddyBearToy extends Toys{
-    constructor(name){
-        super(name);
-    }
-
-    speak(){
-        console.log(`${this.name} says hug me! I am cuddle`);
+class TeddyBearToy extends Toys {
+    speak() {
+        console.log(`${this.name} says hug me! I am cuddle!`);
     }
 }
 
 class DinosaurToy extends Toys {
-    constructor(name){
-        super(name);
-    }
-
-    speak(){
+    speak() {
         console.log(`${this.name} says ROOR!`);
     }
-
 }
-const rusty = new RobotToy("Rusty")
-const fluffy = new TeddyBearToy("Fluffy")
-const rex = new DinosaurToy("Rex")
 
-const toys = [rusty, fluffy, rex]
+// Khởi tạo các đối tượng
+const toys = [
+    new RobotToy("Rusty"),
+    new TeddyBearToy("Fluffy"),
+    new DinosaurToy("Rex")
+];
 
-for (const toy of toys) {
-    toy.speak()
-
-}
+// Thực thi
+toys.forEach(toy => toy.speak());
