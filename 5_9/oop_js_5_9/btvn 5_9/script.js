0395ec389d6daae813abaaeeff3e1aa486cb2acc
KhanhBaoLe <khanhlebao26@gmail.com>
2026-09-09 12:03:53 +0700

    fix

--- 5_9/oop_js_5_9/btvn 5_9/script.js
+++ 5_9/oop_js_5_9/btvn 5_9/script.js
@@ -1,18 +1,16 @@
 class Toys {
     constructor(name) {
-        // Tùy chọn: Ngăn tạo đối tượng trực tiếp từ class Toys (giả lập Abstract Class)
         if (new.target === Toys) {
-            throw new Error("Không thể khởi tạo trực tiếp lớp Toys!");
+            throw new Error("Error!");
         }
         this.name = name;
     }
 
     speak() {
-        throw new Error("Phương thức speak() phải được định nghĩa ở lớp con!");
+        throw new Error("Error!");
     }
 }
 
-// Bỏ constructor dư thừa, JS tự động gọi super(name)
 class RobotToy extends Toys {
     speak() {
         console.log(`${this.name} says beep beep book! I am a robot!`);
@@ -31,12 +29,10 @@ class DinosaurToy extends Toys {
     }
 }
 
-// Khởi tạo các đối tượng
 const toys = [
     new RobotToy("Rusty"),
     new TeddyBearToy("Fluffy"),
     new DinosaurToy("Rex")
 ];
 
-// Thực thi
 toys.forEach(toy => toy.speak());
